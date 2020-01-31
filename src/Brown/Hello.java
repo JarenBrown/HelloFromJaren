274a618dc99de5e0e19bff3b73e9e91317c674de
ToddWyattNelson <nel17052@byui.edu>
2020-01-31 12:19:00 -0700

    Hello From Wyatt

--- src/Brown/Hello.java
+++ src/Brown/Hello.java
@@ -3,5 +3,7 @@ package Brown;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jaren");
+        System.out.println("Hello, my name is Wyatt");
+        
     }
 }
